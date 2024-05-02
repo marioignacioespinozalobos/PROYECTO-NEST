5b637891f7b1498a18e1819b68bf7ee14f5e9314
marioignacioespinozalobos <152904922+marioignacioespinozalobos@users.noreply.github.com>
2024-05-01 20:19:48 -0400

    segunda subidan

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,12 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'prettytier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',  
   },
+  "prettytier/prettier": [
+    "error",
+    {
+      "endOfLine": "auto"
+    }
+    ]
 };
